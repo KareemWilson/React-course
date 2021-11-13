93d465050459d3052107526a88cf1347b562e35d
KareemWilson <kareemmagdywilson@gmail.com>
2021-11-13 23:05:43 +0200

    make a reusable form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./common/input";
+import Form from "./common/form";
 
-class LoginForm extends React.Component {
+class LoginForm extends Form {
   state = {
     data: {
       username: "",
@@ -16,46 +17,6 @@ class LoginForm extends React.Component {
     password: Joi.string().required().label("Password"),
   };
 
-  validate = () => {
-    const option = { abortEarly: false };
-    const { error } = Joi.validate(this.state.data, this.schema, option);
-
-    if (!error) return null;
-
-    const errors = {};
-
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
-    console.log("submitted");
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-    this.setState({ errors });
-
-    const data = { ...this.state.data };
-    data[input.name] = input.value;
-    this.setState({ data });
-  };
   render() {
     const { data, errors } = this.state;
     return (
